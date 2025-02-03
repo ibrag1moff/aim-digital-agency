c46b93358187141bc5559af2b343f62e2c7c337f
Abbas Ibragimov <120020090+ibrag1moff@users.noreply.github.com>
2025-02-03 22:46:58 +0400

    Update

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,7 +3,7 @@ import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "az"],
+  locales: ["az", "en"],
 
   // Used when no locale matches
   defaultLocale: "az",
